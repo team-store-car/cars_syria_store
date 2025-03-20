4d888ad515fd10a4c09ccad36d91e8e1fb9f5ec5
Ahmad Hanifa <ahmad.z.hanifa@gmail.com>
2025-03-21 00:32:24 +0100

    refactor: rename routes/api.php to routes/auth.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
-use App\Http\Middleware\RoleMiddleware; // تأكد من استيراد الـ Middleware
+use App\Http\Middleware\RoleMiddleware; 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/auth.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )

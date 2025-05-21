f2e7cf260f11159420d777051e06ab23e206b526
ibrahem kezzo <ibrahemkezzo.w@gmail.com>
2025-05-21 19:04:03 +0300

    fix:enable seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // Car::factory(20)->create();
-        // User::factory(20)->create();
-        // Category::factory(5)->create();
+        Car::factory(20)->create();
+        User::factory(20)->create();
+        Category::factory(5)->create();
 
     }
 }

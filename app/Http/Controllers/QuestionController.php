8f990632831b05d6694e25c92545d9b24fe3ee69
Ahmad Hanifa <ahmad.z.hanifa@gmail.com>
2025-05-24 11:13:12 +0200

    Fix Error

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\QuestionRepository;
@@ -20,11 +20,25 @@ class QuestionController extends Controller
      */
     public function index(): JsonResponse
     {
-        $questions = $this->questionRepository->getAllWithOptions();
+        try {
+            $questions = $this->questionRepository->getAllWithOptions();
 
-        return response()->json([
-            'status' => true,
-            'data' => $questions
-        ]);
+            if (!$questions || $questions->isEmpty()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'لا يوجد اي سؤال'
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => true,
+                'data' => $questions
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'خطأ في الاتصال بالخادم'
+            ], 500);
+        }
     }
 }

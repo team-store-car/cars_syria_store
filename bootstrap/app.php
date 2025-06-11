9f309a61c56f108357c024e9550866742dd40515
Ahmad Hanifa <ahmad.z.hanifa@gmail.com>
2025-06-11 23:32:15 +0200

    nothing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,15 +24,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api([
             EnsureFrontendRequestsAreStateful::class,
-           \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
-
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
-
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         
